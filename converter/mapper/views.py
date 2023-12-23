9d8fb4006ed082343e6231cc36ff7b575496f0fc
Ilya <pankratovilyasergeevich@gmail.com>
2023-12-24 01:10:55 +0200

    Added import/export data feture + and using Sergey's maper

---
+++ converter/mapper/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .maper import mapping
+
+
+# Create your views here.
+def handle_uploaded_file(f):
+    with open(f"media/input_file.xml", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
+def convert_xml(request):
+    if request.method == "POST":
+        handle_uploaded_file(request.FILES['input_file'])
+        mapping.xml_mapping('media/input_file.xml', 'mapper/maper/stop_list.csv')
+        print('Файл прошел конвертацию!')
+
+    return render(request, 'index.html', {'file': 'media/output.xml'})
